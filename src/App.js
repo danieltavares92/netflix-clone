fdd6341e7a4a11e56b24cdeac75f1c2bc6f6cf41
Renato GUILHERME DE SANT'ANNA SILVA <LR5972@engie.com>
2026-03-01 23:24:57 -0300

    configure vercel function

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Row from './components/Row'
 import Banner from './components/Banner'
+import { useEffect, useState } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/trending")
+      .then(res => res.json())
+      .then(data => setMovies(data.results));
+  }, []);
+
   return (
-    <div className="App">
-      <Navbar/>
-      <Banner/>
-      <Row title='Em alta'/>
-      <Row title='Comédia'/>
-      <Row title='Suspense'/>
+    <div style={{ background: "#000", color: "#fff" }}>
+      <h1>Netflix Clone</h1>
+
+      {movies.map(movie => (
+        <div key={movie.id}>
+          <img
+            src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+            alt={movie.title}
+          />
+          <p>{movie.title}</p>
+        </div>
+      ))}
     </div>
   );
 }
