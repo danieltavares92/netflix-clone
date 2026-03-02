45c3d11d3ec48855590dcd2b54739cb53f232944
Renato GUILHERME DE SANT'ANNA SILVA <LR5972@engie.com>
2026-03-01 23:29:34 -0300

    test api

--- api/trending.js
+++ api/trending.js
@@ -1,8 +1,11 @@
 module.exports = async function (req, res) {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.TMDB_KEY}`
-  );
-
-  const data = await response.json();
-  res.status(200).json(data);
+  res.status(200).json({ message: "API funcionando 🚀" });
 };
+//module.exports = async function (req, res) {
+//  const response = await fetch(
+//    `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.TMDB_KEY}`
+//  );
+
+ // const data = await response.json();
+ // res.status(200).json(data);
+//};
